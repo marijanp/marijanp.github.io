96a6e445bd00be7e87de37a89171c0b51a8d595b
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-08-25 11:37:23 +0200

    flake: flake-utils -> flake-parts, ghc8 -> ghc9

--- flake.nix
+++ flake.nix
@@ -2,30 +2,42 @@
   description = "marijanp's website";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    haskellNix.url = "github:input-output-hk/haskell.nix";
-    nixpkgs.follows = "haskellNix/nixpkgs-2111";
+    haskellNix.url = "github:input-output-hk/haskell.nix/c6b80fe119af19c9c40ffadd41579ff8db675aab";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, flake-utils, nixpkgs, haskellNix }:
-    flake-utils.lib.eachSystem [ "x86_64-darwin" ] (system:
-      let
-        overlays = [ haskellNix.overlay (final: prev: {
-          marijanp = final.haskell-nix.cabalProject {
-            src = ./.;
-            compiler-nix-name = "ghc8107";
-            shell.tools = {
-              cabal = {};
-              hlint = {};
-              haskell-language-server = {};
-            };
-          };
-        })];
-        pkgs = import nixpkgs { inherit system overlays; };
-        flake = pkgs.marijanp.flake { };
-      in flake // {
-        devShells.marijanp = flake.devShell;
-        defaultPackage = flake.packages."marijanp-github-io:exe:site";
-      }
-    );
+  outputs = { self, flake-parts, nixpkgs, haskellNix, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = [ "x86_64-linux" ];
+      imports = [
+      ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          overlays = [
+            haskellNix.overlay
+            (final: prev: {
+              marijanp = final.haskell-nix.cabalProject {
+                src = ./.;
+                compiler-nix-name = "ghc923";
+                shell.tools = {
+                  cabal = { };
+                  hlint = { };
+                  haskell-language-server = { };
+                };
+              };
+            })
+          ];
+          pkgs = import nixpkgs { inherit system overlays; };
+          haskellNixFlake = pkgs.marijanp.flake { };
+        in
+        # remove devShell as it's not supported by flake-parts
+        pkgs.lib.recursiveUpdate (builtins.removeAttrs haskellNixFlake [ "devShell" ]) {
+          packages.default = haskellNixFlake.packages."marijanp-github-io:exe:site";
+          devShells.marijanp = haskellNixFlake.devShell;
+        };
+    };
 }
