a98ee1c69965cd29c29bd9ee69590bf6d398c7fe
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-03-18 16:41:49 -0500

    website: update theme state persistance script

--- docs/js/theme-state.js
+++ docs/js/theme-state.js
@@ -1,9 +1,11 @@
-function loadThemeState(){    
+document.addEventListener('DOMContentLoaded', () => {
   var checked = JSON.parse(localStorage.getItem('themeState'));
   document.getElementById("themeController").checked = checked;
-}
+});
+
 function saveThemeState(){
   var checkbox = document.getElementById('themeController');
   localStorage.setItem('themeState', checkbox.checked);
 }
+
 
--- src/js/theme-state.js
+++ src/js/theme-state.js
@@ -1,7 +1,8 @@
-function loadThemeState(){    
+document.addEventListener('DOMContentLoaded', () => {
   var checked = JSON.parse(localStorage.getItem('themeState'));
   document.getElementById("themeController").checked = checked;
-}
+});
+
 function saveThemeState(){
   var checkbox = document.getElementById('themeController');
   localStorage.setItem('themeState', checkbox.checked);
