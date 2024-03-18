b86b0d7acef4c3b2613375661b1d7d927d4f9f8f
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-03-18 16:18:32 -0500

    website: persist theme state

---
+++ docs/js/theme-state.js
@@ -0,0 +1,9 @@
+function loadThemeState(){    
+  var checked = JSON.parse(localStorage.getItem('themeState'));
+  document.getElementById("themeController").checked = checked;
+}
+function saveThemeState(){
+  var checkbox = document.getElementById('themeController');
+  localStorage.setItem('themeState', checkbox.checked);
+}
+
