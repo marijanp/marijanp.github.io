ad548a76efca4c5745974ef7fd85cd4e78449137
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-07-31 14:58:32 +0200

    use flake.lock in release.nix, bump inputs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "marijan's website";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/8a2f738d9d1f1d986b5a4cd2fd2061a7127237d7";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    npmlock2nix.url = "github:nix-community/npmlock2nix/9197bbf397d76059a76310523d45df10d2e4ca81";
+    npmlock2nix.url = "github:nix-community/npmlock2nix";
     npmlock2nix.flake = false;
   };
 
--- release.nix
+++ release.nix
@@ -1,10 +1,21 @@
 let
-  sources = import ./npins;
-  pkgs = import sources.nixpkgs {
+
+  flakeLock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  getSource = inputName: fetchTarball
+    (
+      let input = flakeLock.nodes.${inputName}.locked;
+      in
+      {
+        url = input.url
+          or "https://github.com/${input.owner}/${input.repo}/archive/${input.rev}.tar.gz";
+        sha256 = input.narHash;
+      }
+    );
+  pkgs = import (getSource "nixpkgs") {
     overlays = [
       (final: prev: {
         nodejs-16_x = final.nodejs;
-        npmlock2nix = pkgs.callPackage sources.npmlock2nix { };
+        npmlock2nix = pkgs.callPackage (getSource "npmlock2nix") { };
       })
       (import ./overlay.nix)
     ];
