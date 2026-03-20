a52e63ea14d9c1c8a6ee62a8c796d8ae9e7b2488
Marijan Petričević <marijan.petricevic94@gmail.com>
2026-03-20 12:16:41 +0100

    site: structure and icon fixes

--- site/js/theme-state.js
+++ site/js/theme-state.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  var checked = JSON.parse(localStorage.getItem('themeState'));
-  document.getElementById("themeController").checked = checked;
-});
-
-function saveThemeState(){
   var checkbox = document.getElementById('themeController');
-  localStorage.setItem('themeState', checkbox.checked);
-}
+  checkbox.checked = JSON.parse(localStorage.getItem('themeState'));
+  checkbox.addEventListener('change', () => {
+    localStorage.setItem('themeState', checkbox.checked);
+  });
+});
